ff0ae4b2c9930b06cd13a97c3280ed798cada6c5
VaLiuM09 <marcello.tridenti@tutanota.com>
2021-07-16 14:15:54 +0200

    Remove menu and modes (#2)
    * Removed UI from default course controller
    
    * Updated default course controller to use simple loader
    
    * Removed menu from standalone course controller
    
    * Removed menu related files, nohints mode

--- Source/Basic-UI-Component/Runtime/CourseController/Controllers/DefaultCourseController.cs
+++ Source/Basic-UI-Component/Runtime/CourseController/Controllers/DefaultCourseController.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using VRBuilder.Core.Input;
 
 namespace VRBuilder.UX
 {
     /// <summary>
     /// Default course controller.
     /// </summary>
-    public class DefaultCourseController : UIBaseCourseController
+    public class DefaultCourseController : BaseCourseController
     {
         /// <inheritdoc />
         public override string Name { get; } = "Default";
@@ -18,12 +19,10 @@ namespace VRBuilder.UX
         public override int Priority { get; } = 50;
 
         /// <inheritdoc />
-        public override string CourseMenuPrefabName { get; } = "CourseControllerMenu";
-
-        /// <inheritdoc />
         public override List<Type> GetRequiredSetupComponents()
         {
             List<Type> requiredSetupComponents = base.GetRequiredSetupComponents();
+            requiredSetupComponents.Add(InputController.ConcreteType);
             requiredSetupComponents.Add(typeof(SpectatorController));
             return requiredSetupComponents;
         }
