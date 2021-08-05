ea63606bc733449b0887e0e8048598a2347ff909
Marcello Tridenti <marcello.tridenti@tutanota.com>
2021-08-05 17:00:28 +0200

    Commented out analytics version checker event

--- Source/Core/Editor/Analytics/VersionCheckerEvent.cs
+++ Source/Core/Editor/Analytics/VersionCheckerEvent.cs
@@ -1,50 +1,50 @@
-using VRBuilder.Editor;
-using VRBuilder.Editor.Analytics;
-using UnityEditor;
-using UnityEngine;
+//using VRBuilder.Editor;
+//using VRBuilder.Editor.Analytics;
+//using UnityEditor;
+//using UnityEngine;
 
-namespace VRBuilder.Core.Editor
-{
-    /// <summary>
-    /// Checks if the version of the VR Builder was updated and sends an event.
-    /// </summary>
-    [InitializeOnLoad]
-    internal static class VersionCheckerEvent
-    {
-        private const string unknownVersionString = "unknown";
+//namespace VRBuilder.Core.Editor
+//{
+//    /// <summary>
+//    /// Checks if the version of the VR Builder was updated and sends an event.
+//    /// </summary>
+//    [InitializeOnLoad]
+//    internal static class VersionCheckerEvent
+//    {
+//        private const string unknownVersionString = "unknown";
 
-        static VersionCheckerEvent()
-        {
-            CheckVersion();
-        }
+//        static VersionCheckerEvent()
+//        {
+//            CheckVersion();
+//        }
 
-        private async static void CheckVersion()
-        {
-            string coreVersion = await EditorUtils.GetCoreVersionAsync();
+//        private async static void CheckVersion()
+//        {
+//            string coreVersion = await EditorUtils.GetCoreVersionAsync();
 
-            if (Application.isBatchMode)
-            {
-                return;
-            }
+//            if (Application.isBatchMode)
+//            {
+//                return;
+//            }
 
-            BuilderProjectSettings settings = BuilderProjectSettings.Load();
-            if (settings == null || string.IsNullOrEmpty(settings.ProjectBuilderVersion))
-            {
-                return;
-            }
+//            BuilderProjectSettings settings = BuilderProjectSettings.Load();
+//            if (settings == null || string.IsNullOrEmpty(settings.ProjectBuilderVersion))
+//            {
+//                return;
+//            }
 
-            if (settings.ProjectBuilderVersion == unknownVersionString || coreVersion == unknownVersionString)
-            {
-                return;
-            }
+//            if (settings.ProjectBuilderVersion == unknownVersionString || coreVersion == unknownVersionString)
+//            {
+//                return;
+//            }
 
-            if (settings.ProjectBuilderVersion != coreVersion)
-            {
-                IAnalyticsTracker tracker = AnalyticsUtils.CreateTracker();
-                tracker.Send(new AnalyticsEvent() { Category = "creator", Action = "updated", Label = coreVersion });
-                settings.ProjectBuilderVersion = coreVersion;
-                settings.Save();
-            }
-        }
-    }
-}
+//            if (settings.ProjectBuilderVersion != coreVersion)
+//            {
+//                IAnalyticsTracker tracker = AnalyticsUtils.CreateTracker();
+//                tracker.Send(new AnalyticsEvent() { Category = "creator", Action = "updated", Label = coreVersion });
+//                settings.ProjectBuilderVersion = coreVersion;
+//                settings.Save();
+//            }
+//        }
+//    }
+//}
