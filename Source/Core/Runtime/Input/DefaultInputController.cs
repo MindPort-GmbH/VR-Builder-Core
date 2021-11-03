72cd55fdb2ac5e4765c5c461109d354f0e9a9f14
Marcello Tridenti <marcello.tridenti@tutanota.com>
2021-11-03 17:07:32 +0100

    Added define for input system package and checking it along with ENABLE_INPUT_SYSTEM

--- Source/Core/Runtime/Input/DefaultInputController.cs
+++ Source/Core/Runtime/Input/DefaultInputController.cs
@@ -1,8 +1,8 @@
-﻿// Copyright (c) 2013-2019 Innoactive GmbH
+// Copyright (c) 2013-2019 Innoactive GmbH
 // Licensed under the Apache License, Version 2.0
 // Modifications copyright (c) 2021 MindPort GmbH
 
-#if ENABLE_INPUT_SYSTEM
+#if ENABLE_INPUT_SYSTEM && INPUT_SYSTEM_PACKAGE
 
 using System;
 using System.Collections.Generic;
